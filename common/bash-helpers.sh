71984f0682ced8a9c3b3dcdf93739ef5499b086e
Rahul Prajapati <rahul.prajapati@live.in>
2021-10-22 13:54:57 +0530

    Add nvm config for .zshrc

--- common/bash-helpers.sh
+++ common/bash-helpers.sh
@@ -69,3 +69,8 @@ docker-cleanup() {
   docker volume prune -f
   docker network prune -f
 }
+
+# nvm config
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
